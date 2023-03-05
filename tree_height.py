6a58ed65f30d0c37b4acc7d212b93d539e1de8dd
LindaSlapina <125659827+LindaSlapina@users.noreply.github.com>
2023-03-05 23:18:26 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -6,12 +6,38 @@ import threading
 
 def compute_height(n, parents):
     # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
-
+    root - parents.index(-1)
+    height = 1 
+    while parents[root] != -1:
+        root = parents[root]
+        height = height +1
+    return height
 
 def main():
+    while True:
+        newInput = input("ievadiet 'I', lai ievadītu no tastatūras vai 'F', lai ievadītu no faila:")
+        if newInput == "I":
+            n = int(input("Ievadiet 'node' skaitu:"))
+            parents = list(map(int,input("ievadiet koka vecāku skaitu:")))
+            break
+        elif newInput == "F":
+            fileName = input("Ievadiet faila nosaukumu:")
+            if "a" in fileName:
+                print("faila nosaukumā nevar būt burts 'a'")
+                return 1
+            try:
+                n = int(fileName.readline())
+                parents = list(map(int, fileName.readline().split()))
+                break
+            except FileNotFoundError:
+                print("fails netika atrasts, pārbaudiet faila pareizrakstību")
+                return 1 
+        else:
+            print("nepareiza izvēla, ievadiet tikai 'I' vai 'F'")
+    height = compute_height(n , parents)
+    print(height)            
+
+
     # implement input form keyboard and from files
     
     # let user input file name to use, don't allow file names with letter a
